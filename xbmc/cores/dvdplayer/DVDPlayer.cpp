fc30b1581c3b9011b69fed46a0f2e491e0151c29
S. Davilla <davilla@4pi.com>
2016-07-01 01:24:26 -0400

    core: more redacting

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -770,7 +770,8 @@ bool CDVDPlayer::OpenInputStream()
   m_pInputStream = CDVDFactoryInputStream::CreateInputStream(this, m_filename, m_mimetype, m_item.ContentLookup());
   if(m_pInputStream == NULL)
   {
-    CLog::Log(LOGERROR, "CDVDPlayer::OpenInputStream - unable to create input stream for [%s]", m_filename.c_str());
+    CLog::Log(LOGERROR, "CDVDPlayer::OpenInputStream - unable to create input stream for [%s]",
+      CURL::GetRedacted(m_filename.c_str()).c_str());
     return false;
   }
   else
@@ -778,7 +779,8 @@ bool CDVDPlayer::OpenInputStream()
 
   if (!m_pInputStream->Open(m_filename.c_str(), m_mimetype, m_item.ContentLookup()))
   {
-    CLog::Log(LOGERROR, "CDVDPlayer::OpenInputStream - error opening [%s]", m_filename.c_str());
+    CLog::Log(LOGERROR, "CDVDPlayer::OpenInputStream - error opening [%s]",
+      CURL::GetRedacted(m_filename.c_str()).c_str());
     return false;
   }
 
