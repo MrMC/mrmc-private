b30edfd76c8265ff955329a9d69374bb5be01716
amet <amet.nospam@gmail.com>
2017-03-22 14:06:53 +0400

    plex: fix typo

--- xbmc/filesystem/PlexDirectory.cpp
+++ xbmc/filesystem/PlexDirectory.cpp
@@ -154,7 +154,7 @@ bool CPlexDirectory::GetDirectory(const CURL& url, CFileItemList &items)
       else if (path == "inprogressmovies")
       {
         CPlexUtils::GetPlexInProgressMovies(items, Base64::Decode(section));
-        items.SetLabel(g_localizeStrings.Get(20386));
+        items.SetLabel(g_localizeStrings.Get(627));
         items.SetContent("movies");
       }
       else
