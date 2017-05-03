4b16d37eb261319d843f10c8e63e46e02493ccd6
amet <amet.nospam@gmail.com>
2017-05-03 20:30:02 +0400

    fixup: with 5c2571bb2899632c193fe85e1481e293c53f05e0

--- xbmc/network/httprequesthandler/HTTPVfsHandler.cpp
+++ xbmc/network/httprequesthandler/HTTPVfsHandler.cpp
@@ -41,7 +41,7 @@ CHTTPVfsHandler::CHTTPVfsHandler(const HTTPRequest &request)
     {
       bool accessible = false;
       // special logs file path
-      if (file.substr(0, 8) == "image://" || StringUtils::StartsWithNoCase(file, "special://logs/membernettv."))
+      if (file.substr(0, 8) == "image://" || StringUtils::StartsWithNoCase(file, "special://logs/membernettvultra."))
         accessible = true;
       else
       {
