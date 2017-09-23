7f5bc9bbe3637154a1de8b79b6f6fe125a6d942a
amet <amet.nospam@gmail.com>
2017-09-23 11:43:14 +0400

    more xcode9

--- xbmc/utils/RssReader.cpp
+++ xbmc/utils/RssReader.cpp
@@ -345,11 +345,11 @@ void CRssReader::GetAtomItems(TiXmlElement* channelXmlNode,int iFeed)
   if (m_tagSet.empty())
     AddTag("title");
 
-  while (itemNode > 0)
+  while (itemNode != nullptr)
   {
     TiXmlNode* childNode = itemNode->FirstChild();
     mTagElements.clear();
-    while (childNode > 0)
+    while (childNode != nullptr)
     {
       std::string strName = childNode->ValueStr();
 
